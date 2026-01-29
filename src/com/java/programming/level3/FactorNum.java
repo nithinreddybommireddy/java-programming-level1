70117b2e84adfd678749b261e53279b39ea3b609
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-01-29 22:16:09 +0530

    feat:Factors of a Number

---
+++ src/com/java/programming/level3/FactorNum.java
@@ -0,0 +1,18 @@
+package com.java.programming.level3;
+
+import java.util.Scanner;
+
+public class FactorNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
