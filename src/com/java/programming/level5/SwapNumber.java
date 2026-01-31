bbfdaa6ce5d6ffc33030380d2bb6fe0321df2265
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-01-31 22:23:44 +0530

    feat:  Swapping Numbers without using extra variable

---
+++ src/com/java/programming/level5/SwapNumber.java
@@ -0,0 +1,21 @@
+package com.java.programming.level5;
+
+import java.util.Scanner;
+
+public class SwapNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+
