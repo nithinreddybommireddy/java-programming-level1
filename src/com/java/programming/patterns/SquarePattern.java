0663d4afcce10ad98fef8344cffd1df9dd0c785f
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-04 14:11:26 +0530

    feat: Square Pattern

--- src/com/java/programming/patterns/SquarePattern.java
+++ src/com/java/programming/patterns/SquarePattern.java
@@ -6,7 +6,7 @@ public class SquarePattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter a number");
+        System.out.println("Enter a number :");
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
