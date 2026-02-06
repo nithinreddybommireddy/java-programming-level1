7a6a93702ca479659315b25b9991b60f5efa3b9e
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-06 22:51:42 +0530

    feat:Even numbers  1 to N

--- src/com/java/programming/level1/Even1toN.java
+++ src/com/java/programming/level1/Even1toN.java
@@ -6,7 +6,7 @@ public class Even1toN {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        System.out.print("enter number:");
+        System.out.print("Enter number:");
         int n=sc.nextInt();
         System.out.println("Even number upto :" +n);
 
