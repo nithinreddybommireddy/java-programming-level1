ae0ad37f6fb779dee2cf362c3af9e86d4140049d
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-06 22:53:32 +0530

    feat: Odd numbers 1 to N

--- src/com/java/programming/level1/Odd1toN.java
+++ src/com/java/programming/level1/Odd1toN.java
@@ -7,7 +7,7 @@ public class Odd1toN
     public static void main(String[] args)
 {
     Scanner sc=new Scanner(System.in);
-    System.out.print("enter number:");
+    System.out.print("Enter number:");
     int n=sc.nextInt();
     System.out.println("Odd number upto :" +n);
 
