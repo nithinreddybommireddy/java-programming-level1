3a73168940b455396ccb5df1aef6cd62710b6ec7
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-08 23:08:49 +0530

    feat:Sum Of Odd Numbers 1 to N

---
+++ src/com/java/programming/level1/SumOfOddNum1toN.java
@@ -0,0 +1,22 @@
+package com.java.programming.level1;
+
+import java.util.Scanner;
+
+public class SumOfOddNum1toN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number:");
+        int n = sc.nextInt();
+        System.out.println(" Sum of Odd number upto :" + n);
+        int oddsum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                oddsum = oddsum + i;
+            }
+
+        }
+        System.out.println(oddsum);
+        sc.close();
+    }
+
+}
